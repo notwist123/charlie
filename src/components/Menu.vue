ba71c336baaf9cdabd0c2925c93358ac03d4bdd0
CharlieHuang <charlie.huang@titansoft.com.sg>
2018-12-25 02:52:22 +0800

    Commit folders that didn't add last time

---
+++ src/components/Menu.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <Menu mode="horizontal" :theme="theme1" active-name="1">
+        <Menu-Item name="1">
+            <Icon type="ios-home" />
+            Home
+        </Menu-Item>
+        <Menu-Item name="2">
+            <Icon type="ios-people" />
+            User
+        </Menu-Item>
+        <Menu-Item name="4">
+            <Icon type="ios-paper" />
+            Post
+        </Menu-Item>
+    </Menu>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MenuBar',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,32 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import VueRouter from 'vue-router'
+import Vuex from 'vuex'
+import iView from 'iview'
+import 'iview/dist/styles/iview.css'
+
+import MenuBar from '@/components/Menu'
+
+Vue.config.productionTip = false
+
+Vue.use(iView)
+Vue.use(Vuex)
+Vue.use(VueRouter)
+
+const routes = [
+  { path: '/', component: MenuBar }
+]
+
+const router = new VueRouter({
+  routes
+})
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
